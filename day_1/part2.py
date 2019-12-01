45e66119e312fd184df01ac00a25438ed385c51a
Jacob Hagstedt <jacob.hagstedt@lamneasoftware.se>
2019-12-01 17:11:31 +0100

    Add day 1 part 2

--- day_1/part2.py
+++ day_1/part2.py
@@ -1,9 +1,20 @@
-def caculate_fuel(mass):
+def calculate_fuel(mass):
     fuel = mass // 3 - 2
     if fuel <= 0:
         return 0
-    return fuel + caculate_fuel(fuel)
+    return fuel + calculate_fuel(fuel)
 
-if __name__ == "__maain__":
-    
 
+def _read_masses(path):
+    with open(path, "r") as f:
+        return list(map(int, f.read().split("\n")))
+
+
+def caculate_total_fuel(masses):
+    return sum(map(calculate_fuel, masses))
+
+
+if __name__ == "__main__":
+    masses = _read_masses("input.txt")
+    fuel_requires = caculate_total_fuel(masses)
+    print(len(masses), "masses require", fuel_requires, "fuel")
--- tests/day_1/test_part2.py
+++ tests/day_1/test_part2.py
@@ -6,12 +6,14 @@ def test_calculate_fuel():
     output_calculate_total_fuel = [2, 966, 50346]
 
     for x, y in zip(input_calculate_total_fuel, output_calculate_total_fuel):
-        assert day_1.part1.calculate_fuel(x) == y
+        assert day_1.part2.calculate_fuel(x) == y
 
 
 def caculate_total_fuel():
     input_calculate_total_fuel = [12, 14, 1969, 100756]
-    output_calculate_total_fuel = [2, 2, 654, 33583]
+    output_calculate_total_fuel = 51314
 
-    for x, y in zip(input_calculate_total_fuel, output_calculate_total_fuel):
-        assert day_1.part1.caculate_total_fuel([x]) == y
+    assert (
+        day_1.part2.caculate_total_fuel(input_calculate_total_fuel)
+        == output_calculate_total_fuel
+    )
