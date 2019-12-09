7e482b32697d8854d2882e88a85f260ee379445f
Jacob Hagstedt <jacob.hagstedt@lamneasoftware.se>
2019-12-09 19:14:27 +0100

    Add day 8 part 1

--- day_8/part1.py
+++ day_8/part1.py
@@ -1,56 +1,41 @@
 from collections import defaultdict
 import operator
 
+
 def _read(path):
     with open(path, "r") as f:
         return f.read()
 
 
-def main(image):
-    layers = list()
+def group_list(items, group_size):
+    group = None
+    for i, item in enumerate(items):
+        if i % group_size == 0:
+            if group is not None:
+                yield group
+            group = list()
+        group.append(item)
+    yield group
 
+
+def main(image):
     width = 25
     height = 6
-    j = 0
-    min_layer = 0
-    max_numnber = 0
-    for index, i in enumerate(range(0, len(image), width)):
-        batch = image[i : i + width]
 
-        layers.append((j, i, batch.count("0")))
+    min_number = 9999
+    saved_batch = None
+    for layer_index, batch in enumerate(group_list(image, width * height)):
 
         count = batch.count("0")
-        if count >= max_numnber:
-            min_layer = j
-            max_numnber = count
-            print("Batch:", batch)
+        if count < min_number:
+            min_number = count
+            saved_batch = batch
 
-        if index > 0 and index%height == 0:
-            print("New layer")
-            j += 1
-
-        print("Layer", (j+1), "Batch:", batch)
-
-
-    print(layers)
-    layers.sort(key=operator.itemgetter(2), reverse=True)
-    layers.sort(key=operator.itemgetter(0), reverse=False)
-    print("layers:", layers)
-    l = layers[0][0]
-    print("Best layer:", l+1)
-    l = 98*6
-    print("Numbers:", image[l: l+width])
-    print("Numbers:", image[l+1: l+1+width])
-
-    print("min_layer:", min_layer)
-    print("count:", max_numnber)
+    ones = saved_batch.count("1")
+    twos = saved_batch.count("2")
+    print("Result:", ones * twos)
 
 
 if __name__ == "__main__":
     image = _read("input.txt")
-    print("Len:", len(image))
-    # exit(0)
-    # image = "123456789012"
-
-    # Layer 299 is too low
     main(image)
